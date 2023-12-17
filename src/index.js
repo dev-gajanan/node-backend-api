7e9cb0beae401bd0d82a9d09a57012d28d318267
Gajanan Mallik <gajanan.mallik@tdcx.com>
2023-12-18 00:02:01 +0530

    config for middlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    const serverPort = process.env.PORT || 8000;
+    app.listen(serverPort, () => {
+      console.log(`Application running on port: ${serverPort}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed!!", err);
+  });
