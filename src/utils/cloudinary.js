814286b0899dd4c8f1415b5befc7c92c5135fa6a
Gajanan Mallik <gajanan.mallik@tdcx.com>
2023-12-20 23:26:14 +0530

    Coudinary and multer config added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDE_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file in cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File has been uploaded ", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the local file from server
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
